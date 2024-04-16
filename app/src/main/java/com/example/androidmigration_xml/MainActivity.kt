8b1cf21d42dad5ad39d3db359850cf230bd5b5f2
Taha Jadid <jadid.taha@waroom.com>
2024-04-16 14:37:27 +0100

    refactor code

--- app/src/main/java/com/example/androidmigration_xml/MainActivity.kt
+++ app/src/main/java/com/example/androidmigration_xml/MainActivity.kt
@@ -1,9 +1,6 @@
 package com.example.androidmigration_xml // ktlint-disable package-name
 
 import android.os.Bundle
-import android.os.Handler
-import android.view.View
-import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -23,26 +20,7 @@ class MainActivity : AppCompatActivity() {
      * function that init the UI Components
      */
     private fun initComponents() {
-        titleBottom = findViewById(R.id.bottom_title)
-        imageCenter = findViewById(R.id.center_image)
-
-        imageCenter.visibility = View.GONE
-
-        Handler().postDelayed({
-            imageCenter.visibility = View.VISIBLE
-            val animation = AnimationUtils.loadAnimation(this, R.anim.fade_in)
-            imageCenter.startAnimation(animation)
-        }, 100)
-
-        Handler().postDelayed({
-            val animationOut = AnimationUtils.loadAnimation(this, R.anim.fade_out)
-            imageCenter.startAnimation(animationOut)
-            titleBottom.startAnimation(animationOut)
-        }, 3000)
-
-        Handler().postDelayed({
-            imageCenter.visibility = View.GONE
-            titleBottom.visibility = View.GONE
-        }, 4000)
+        //titleBottom = findViewById(R.id.bottom_title)
+        //imageCenter = findViewById(R.id.center_image)
     }
 }
